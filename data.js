cb133bc2aa69ecb36d9a2b39a976f3ef225abecf
ajaySahu <ajay@sahu.edu.in>
2022-11-03 23:21:14 +0530

    third commit

---
+++ data.js
@@ -0,0 +1,174 @@
+const data={
+    "newarr": [],
+    "total": 21,
+    "users": [
+        {
+            "_id": "6363d5f401d69ba15a0c44b5",
+            "name": "ajay sahu",
+            "age": 24,
+            "mileage": 200,
+            "class": 3,
+            "__v": 0
+        },
+        {
+            "_id": "6363d60e01d69ba15a0c44b8",
+            "name": "anuj sahu",
+            "age": 45,
+            "mileage": 200,
+            "class": 6,
+            "__v": 0
+        },
+        {
+            "_id": "6363d62101d69ba15a0c44ba",
+            "name": "ram sahu",
+            "age": 45,
+            "mileage": 2001,
+            "class": 6,
+            "__v": 0
+        },
+        {
+            "_id": "6363d64201d69ba15a0c44bc",
+            "name": "ram sahu",
+            "age": 30,
+            "mileage": 33456,
+            "class": 10,
+            "__v": 0
+        },
+        {
+            "_id": "6363d65e01d69ba15a0c44be",
+            "name": "chaman sahu",
+            "age": 27,
+            "mileage": 14555,
+            "class": 12,
+            "__v": 0
+        },
+        {
+            "_id": "6363d68401d69ba15a0c44c0",
+            "name": "simran sahu",
+            "age": 19,
+            "mileage": 1300,
+            "class": 5,
+            "__v": 0
+        },
+        {
+            "_id": "6363d69e01d69ba15a0c44c2",
+            "name": "twinkle sahu",
+            "age": 19,
+            "mileage": 1300,
+            "class": 2,
+            "__v": 0
+        },
+        {
+            "_id": "6363d6cf01d69ba15a0c44c5",
+            "name": "ajay sahu",
+            "age": 24,
+            "mileage": 200,
+            "class": 5,
+            "__v": 0
+        },
+        {
+            "_id": "6363d6d401d69ba15a0c44c7",
+            "name": "ajay sahu",
+            "age": 24,
+            "mileage": 200,
+            "class": 5,
+            "__v": 0
+        },
+        {
+            "_id": "6363d6e301d69ba15a0c44c9",
+            "name": "ajay sahu",
+            "age": 34,
+            "mileage": 200,
+            "class": 5,
+            "__v": 0
+        },
+        {
+            "_id": "6363d6e901d69ba15a0c44cb",
+            "name": "ajay sahu",
+            "age": 34,
+            "mileage": 500,
+            "class": 5,
+            "__v": 0
+        },
+        {
+            "_id": "6363d6f201d69ba15a0c44cd",
+            "name": "ajay sahu",
+            "age": 34,
+            "mileage": 20,
+            "class": 5,
+            "__v": 0
+        },
+        {
+            "_id": "6363d6f901d69ba15a0c44cf",
+            "name": "ajay sahu",
+            "age": 34,
+            "mileage": 3200,
+            "class": 5,
+            "__v": 0
+        },
+        {
+            "_id": "6363d70401d69ba15a0c44d1",
+            "name": "ajay sahu",
+            "age": 34,
+            "mileage": 3209,
+            "class": 5,
+            "__v": 0
+        },
+        {
+            "_id": "6363d70b01d69ba15a0c44d3",
+            "name": "chman sahu",
+            "age": 34,
+            "mileage": 3209,
+            "class": 5,
+            "__v": 0
+        },
+        {
+            "_id": "6363d71801d69ba15a0c44d5",
+            "name": "chman sahu",
+            "age": 78,
+            "mileage": 3209,
+            "class": 5,
+            "__v": 0
+        },
+        {
+            "_id": "6363d71d01d69ba15a0c44d7",
+            "name": "chman sahu",
+            "age": 78,
+            "mileage": 3009,
+            "class": 5,
+            "__v": 0
+        },
+        {
+            "_id": "6363d72401d69ba15a0c44d9",
+            "name": "chman sahu",
+            "age": 78,
+            "mileage": 2300,
+            "class": 5,
+            "__v": 0
+        },
+        {
+            "_id": "6363d73c01d69ba15a0c44db",
+            "name": "chman sahu",
+            "age": 78,
+            "mileage": 5000,
+            "class": 5,
+            "__v": 0
+        },
+        {
+            "_id": "6363d76001d69ba15a0c44dd",
+            "name": "chaman sahu",
+            "age": 78,
+            "mileage": 5000,
+            "class": 5,
+            "__v": 0
+        },
+        {
+            "_id": "6363d76101d69ba15a0c44df",
+            "name": "chaman sahu",
+            "age": 78,
+            "mileage": 5000,
+            "class": 5,
+            "__v": 0
+        }
+    ]
+}
